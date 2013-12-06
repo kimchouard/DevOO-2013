f0dc764c92ff24687f4c43e5fe5435b04c2a23ae
Breynat Leslie <leslie.breynat@gmail.com>
2013-12-06 13:00:49 +0100

    Troncon ok

--- src/devoo/h4301/model/Plan.java
+++ src/devoo/h4301/model/Plan.java
@@ -51,36 +51,51 @@ public class Plan {
         this.troncons.add(troncon);
     }
     
-
-//public Noeud getNoeudById(Integer id){}
+    public Noeud getNoeudById(Integer id){
+    
+        Plan plan = getInstance();
+        Noeud noeudCherche = null;
+        ArrayList<Noeud> list = plan.getNoeuds();
+        for (int i=0; i<list.size(); i++){
+            if (list.get(i).id == id){
+                noeudCherche = list.get(i);
+            }
+        }
+        return noeudCherche;
+    }
+    
     public int construireAPartirDomXML(Element racine) {
 
 
         Plan plan = getInstance();
 // Traitement des noeuds
         NodeList list = racine.getElementsByTagName("Noeud");
-        System.out.println("traitement des noeuds");
+      
 
         for (int i = 0; i < list.getLength(); i++) {
             Element noeudElem = (Element) list.item(i);
             Noeud noeudNouveau = new Noeud();
             System.out.println("noeud ajouté");
             noeudNouveau.construireAPartirDomXML(noeudElem);
-//todo : Ajout du noeud noeudà la liste de noeud du plan
             plan.addNoeud(noeudNouveau);
         }
-        System.out.println("traitement des noeuds fini");
 
         String tag = "TronconSortant";
         for (int i = 0; i < list.getLength(); i++) {
             Element noeudElem = (Element) list.item(i);
             NodeList listeTroncon = noeudElem.getElementsByTagName(tag);
-
+            
+            Integer idOrigine = Integer.parseInt(noeudElem.getAttribute("id"));
+            Noeud origine = getNoeudById(idOrigine);
 //Pour chaque noeud, on récupère sa liste de troncon
             for (int j = 0; j < listeTroncon.getLength(); j++) {
                 Element tronconElem = (Element) listeTroncon.item(j);
                 Troncon tronconNouveau = new Troncon();
+                
+                
+                tronconNouveau.setOrigine(origine);
                 tronconNouveau.construireAPartirDomXML(tronconElem);
+                
 // todo : Ajout du troncon à la liste de troncon du plan
 //troncons.add(tronconNouveau);
             }
