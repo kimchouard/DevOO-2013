838ca88f80f6e6d69a21ed89e123502116d92070
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-12-06 12:34:13 +0100

    Update javadoc

--- src/devoo/h4301/controller/LecteurXml.java
+++ src/devoo/h4301/controller/LecteurXml.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package devoo.h4301.controller;
 
@@ -20,52 +20,52 @@ import org.w3c.dom.NodeList;
  * @author Leslie Breynat
  */
 public class LecteurXml {
-
+    
     /**
-     *
+     * Constructeur
      */
     public LecteurXml() {
     }
-
+    
     /**
-     *
-     * @return
+     * Ouvre un fichier XML
+     * @return fichier ouvert
      */
     public File ouvrirFichier(){
         File xml = new File("C:/Users/Leslie Breynat/Desktop/plan10x10.xml");
-                return xml;
+        return xml;
     }
-
+    
     /**
-     *
-     * @return
+     * Construction d'un plan a partir d'un fichier XML
+     * @return plan créé
      */
     public Plan construirePlanAParirXML(){
-Plan plan = new Plan();
-
-File planXML = ouvrirFichier();
-
-if (planXML != null) {
-              try {
-                 // creation d'un constructeur de documents a l'aide d'une fabrique
+        Plan plan = new Plan();
+        
+        File planXML = ouvrirFichier();
+        
+        if (planXML != null) {
+            try {
+                // creation d'un constructeur de documents a l'aide d'une fabrique
                 DocumentBuilder constructeur = DocumentBuilderFactory.newInstance().newDocumentBuilder();
                 // lecture du contenu d'un fichier XML avec DOM
                 Document document = constructeur.parse(planXML);
                 Element racine = document.getDocumentElement();
-if (racine.getNodeName().equals("Reseau")) {
- System.out.println("debut de la construction du plan");
+                if (racine.getNodeName().equals("Reseau")) {
+                    System.out.println("debut de la construction du plan");
 //Passer le plan a la fabrique de plan a partir de domxml
-plan.construireAPartirDomXML(racine);
+                    plan.construireAPartirDomXML(racine);
 //Gerer le cas de pb de lecture de fichier
-
-}
-}	catch (Exception e) {
+                    
+                }
+            }	catch (Exception e) {
                 System.out.println(e.getMessage());}
-
-
-
-}
-return plan;
-
-}
+            
+            
+            
+        }
+        return plan;
+        
+    }
 }
